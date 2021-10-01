114c78659a561d65833b17091dc00d3c43840168
Yana Mulyana <mulyanasmkwd@gmail.com>
2021-10-02 00:30:30 +0700

    formadmin

--- resources/views/admin/exam-report.blade.php
+++ resources/views/admin/exam-report.blade.php
@@ -2,6 +2,7 @@
 @section('title', 'PPDB - Nilai Akhir')
 @section('css')
 <link rel="stylesheet" type="text/css" href="{{asset('admin-panel/js/datatables/css/dataTables.dataTables.css')}}">
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/2.0.1/css/buttons.dataTables.min.css">
 @endsection
 @section('content')
 <div class="app-page-title">
@@ -33,11 +34,12 @@
         <div class="main-card mb-3 card">
             <div class="card-header">
                 NILAI UJIAN
-                <div class=" pr-3 btn-actions-pane-right">
+                {{-- <div class=" pr-3 btn-actions-pane-right">
                     <a href="" class=" btn btn-info">Cetak Hasil Ujian</a>
-                </div>
+                </div> --}}
             </div>
-            <div class="table-responsive">
+            <br>
+            <div class="table-responsive container ">
                 <table id="report-ujian" class="align-middle mb-0 table-striped table-borderless">
                     <thead>
                         <tr>
@@ -69,10 +71,36 @@
 </div>
 @endsection
 
+
 @section('custom_js')
+<script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.0.1/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.print.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.colVis.min.js"></script>
 <script>
     $(document).ready(function() {
-        $('#report-ujian').DataTable();
+        $('#report-ujian').DataTable({
+            dom: 'Bfrtip',
+            buttons: [
+                {
+                    extend: 'excelHtml5',
+                    exportOptions: {
+                        columns: ':visible'
+                    }
+                },
+                {
+                    extend: 'pdfHtml5',
+                    exportOptions: {
+                        columns: ':visible'
+                    }
+                },
+                'colvis'
+            ]
+        });
     });
 </script>
 @endsection
