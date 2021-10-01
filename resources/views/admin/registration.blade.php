bcdf560583858cf9c0fc5e437ab1889d4e8438c3
nooravipp-ux <nooravipp9@gmail.com>
2021-10-01 23:51:08 +0700

    add pdf export

--- resources/views/admin/registration.blade.php
+++ resources/views/admin/registration.blade.php
@@ -3,6 +3,7 @@
 
 @section('css')
 <link rel="stylesheet" type="text/css" href="{{asset('admin-panel/js/datatables/css/dataTables.dataTables.css')}}">
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/2.0.1/css/buttons.dataTables.min.css">
 @endsection
 
 @section('content')
@@ -36,7 +37,7 @@
             </div> <br>
             <div class="container">
                 <div class="table">
-                    <table id="pendaftaran" class="align-middle mb-0 table-striped table-borderless">
+                    <table id="pendaftaran" class="display nowrap align-middle mb-0 table-striped table-borderless">
                         <thead>
                             <tr>
                                 <th class="">No </th>
@@ -83,9 +84,21 @@
 @endsection
 
 @section('custom_js')
+<script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.0.1/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.0.1/js/buttons.print.min.js"></script> 
 <script>
     $(document).ready(function() {
-        $('#pendaftaran').DataTable();
+        $('#pendaftaran').DataTable({
+            dom: 'Bfrtip',
+            buttons: [
+                'excel', 'pdf', 'print'
+            ]
+        });
     });
 </script>
 @endsection
